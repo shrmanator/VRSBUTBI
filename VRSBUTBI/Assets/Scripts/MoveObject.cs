f4d5341f88178048313638b529977b030fd00fda
andemar3 <andemar3@oregonstate.edu>
2022-11-12 19:00:48 -0800

    Testing object movement
    Created CreateMovingCube script which spawns a moving cube at runtime
    Created MoveObject script which causes the object to move horizontally
    Created TestScene to test these scripts

---
+++ VRSBUTBI/Assets/Scripts/MoveObject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    private float movementSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update the position
+        transform.position = transform.position + new Vector3(movementSpeed * Time.deltaTime, 0, 0);
+    }
+}
