7569dc6c67acacb2fac2b4e508affa76e5d8d4f2
ChrisPark <83894916+dhcp0327@users.noreply.github.com>
2022-12-01 12:00:34 +0900

    Text parsing environment setup
    environment set up update for the Text parsing

---
+++ VRSBUTBI/Assets/Scripts/textparser.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class textparser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //"read in file and parse user's commands"
+    void Start()
+    {
+        using (StreamReader sr = File.OpenText(fileName))
+
+        {
+
+            string s = sr.ReadToEnd();
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
