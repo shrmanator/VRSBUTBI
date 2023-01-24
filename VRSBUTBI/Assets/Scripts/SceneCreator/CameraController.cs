88af782c9927c745fe3affab760ba0995092a40f
shrmanator <dovindustries@gmail.com>
2023-01-24 14:43:23 -0500

    Removed mouse panning, added arrow key panning.

--- VRSBUTBI/Assets/Scripts/SceneCreator/CameraController.cs
+++ VRSBUTBI/Assets/Scripts/SceneCreator/CameraController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class RTSCamera : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
     [SerializeField] float panSpeed = 20f;
     [SerializeField] float zoomSpeed = 50f;
@@ -9,36 +9,35 @@ public class RTSCamera : MonoBehaviour
     [SerializeField] float zoomMin = 10f;
     [SerializeField] float zoomMax = 80f;
 
-    [SerializeField] float panBorderThickness = 10f;
-
     void Update()
     {
         Vector3 pos = transform.position;
 
-        // Panning
-        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
-        {
-            pos.z += panSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
-        {
-            pos.z -= panSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
-        {
-            pos.x += panSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
-        {
-            pos.x -= panSpeed * Time.deltaTime;
-        }
 
-        // Zooming
+        // ====== PANNING THE CAMERA ======
+        //
+        if (Input.GetKey("w") || Input.GetKey("up"))
+            pos.z += panSpeed * Time.deltaTime;
+
+        if (Input.GetKey("s") || Input.GetKey("down"))
+            pos.z -= panSpeed * Time.deltaTime;
+
+        if (Input.GetKey("d") || Input.GetKey("right"))
+            pos.x += panSpeed * Time.deltaTime;
+
+        if (Input.GetKey("a") || Input.GetKey("left"))
+            pos.x -= panSpeed * Time.deltaTime;
+
+
+        // ====== ZOOMING IN AND OUT ======
+        //
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         pos.y -= scroll * zoomSpeed * 100f * Time.deltaTime;
         pos.y = Mathf.Clamp(pos.y, zoomMin, zoomMax);
 
-        // Rotation
+
+        // ====== ROTATING THE CAMERA ======
+        //
         if (Input.GetMouseButton(1))
         {
             transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime);
