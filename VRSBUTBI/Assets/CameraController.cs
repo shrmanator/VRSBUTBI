3194754a5a6920f55a3f83621bb026152169321f
shrmanator <dovindustries@gmail.com>
2023-02-01 16:13:03 -0500

    Update CameraController.cs
    Switched commenting style to XML and added some comments

--- VRSBUTBI/Assets/CameraController.cs
+++ VRSBUTBI/Assets/CameraController.cs
@@ -1,23 +1,38 @@
-/*
-Property of VRSBUTBI
-
-This script implements the
-keyboard camera controls
-*/
-
-
 using UnityEngine;
 
+/// <summary>
+/// Property of VRSBUTBI. 
+/// This script implements the keyboard camera controls.
+/// </summary>
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] float panSpeed = 20f;
-    [SerializeField] float zoomSpeed = 50f;
-    [SerializeField] float mouseSensitivity = 100.0f;
+    [SerializeField] 
+    /// <summary>
+    /// Speed at which camera pans horizontally or vertically.
+    /// </summary>
+    float panSpeed = 20f;
+    [SerializeField] 
+    /// <summary>
+    /// Speed at which camera zooms in or out.
+    /// </summary>
+    float zoomSpeed = 50f;
+    [SerializeField] 
+    /// <summary>
+    /// Sensitivity of the mouse movement for rotation.
+    /// </summary>
+    float mouseSensitivity = 100.0f;
+    [SerializeField] 
+    /// <summary>
+    /// Minimum limit of the zoom level.
+    /// </summary>
+    float zoomMin = 2f;
+    [SerializeField] 
+    /// <summary>
+    /// Maximum limit of the zoom level.
+    /// </summary>
+    float zoomMax = 10f;
 
-    [SerializeField] float zoomMin = 2f;
-    [SerializeField] float zoomMax = 10f;
-
-    void Update()
+    private void Update()
     {
         Vector3 cameraPosition = transform.position;
 
@@ -25,27 +40,21 @@ public class CameraController : MonoBehaviour
         //
         if (Input.GetKey("w") || Input.GetKey("up"))
             cameraPosition += transform.forward * panSpeed * Time.deltaTime;
-
         if (Input.GetKey("s") || Input.GetKey("down"))
             cameraPosition -= transform.forward * panSpeed * Time.deltaTime;
-
         if (Input.GetKey("d") || Input.GetKey("right"))
             cameraPosition += transform.right * panSpeed * Time.deltaTime;
-
         if (Input.GetKey("a") || Input.GetKey("left"))
             cameraPosition -= transform.right * panSpeed * Time.deltaTime;
-
 
         // ====== VERTICAL CAMERA MOVEMENT ======
         //
         if (Input.GetKey("e") || Input.GetKey("/"))
             // vertical up movement
             cameraPosition += transform.up * panSpeed * Time.deltaTime;
-
         if (Input.GetKey("f") || Input.GetKey(KeyCode.RightAlt))
             // vertical down movement
             cameraPosition -= transform.up * panSpeed * Time.deltaTime;
-
 
         // ====== ZOOMING IN AND OUT ======
         //
@@ -53,17 +62,16 @@ public class CameraController : MonoBehaviour
         cameraPosition.y -= scroll * zoomSpeed * 100f * Time.deltaTime;
         cameraPosition.y = Mathf.Clamp(cameraPosition.y, zoomMin, zoomMax);
 
-
         // ====== ROTATING THE CAMERA ======
         //
         if (Input.GetMouseButton(1))
         {
             float mouseX = Input.GetAxis("Mouse X");
             float mouseY = Input.GetAxis("Mouse Y");
-            Quaternion rotation = Quaternion.Euler(transform.localEulerAngles.x - mouseY * mouseSensitivity * Time.deltaTime, transform.localEulerAngles.y + mouseX * mouseSensitivity * Time.deltaTime, 0);
-            transform.localRotation = rotation;
-        }
-
-        transform.position = cameraPosition;
-    }
-}
+            // Rotation on the x-axis based on mouse movement in the vertical direction
+            float xRotation = transform.localEulerAngles.x - mouseY * mouseSensitivity * Time.deltaTime;
+            // Rotation on the y-axis based on mouse movement in the horizontal direction
+            float yRotation = transform.localEulerAngles.y
+         }
+      }
+  }
