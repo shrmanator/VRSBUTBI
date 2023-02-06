5c2ce152b2dc1c01f8e154cb2eb88ce266ed9f72
andemar3 <andemar3@oregonstate.edu>
2023-02-06 15:18:22 -0800

    Added importLibrary functionality
    known issues: the user is not prompted for a file for more object types and the model from the first object type is used instead

--- VRSBUTBI/Assets/ObjectCreator.cs
+++ VRSBUTBI/Assets/ObjectCreator.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Collections;
@@ -12,30 +13,19 @@ using SimpleFileBrowser;
 namespace ObjectCreator {
 public sealed class ObjCreator : MonoBehaviour
 {
-   // Dictionary<string, string> importLibrary = new Dictionary<string, string>();
-    string objPath = string.Empty;
+    Dictionary<string, GameObject> importLibrary = new Dictionary<string, GameObject>();
     string error = string.Empty;
     //file path for testing. Remove when dialogue prompt is implemented
     public string filePath = string.Empty;
-
+    OBJLoader objLoader = new OBJLoader();
     private GameObject loadedObject;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     //creates objects from a list of objects
     public void CreateObjects(string [,] objectsList)
     {
         for (int i = 0; i < objectsList.GetLength(0); i++)
         {
+            UnityEngine.Debug.Log("New object");
             string[] objectData = new string[] {objectsList[i, 0], objectsList[i, 1], objectsList[i, 2]};
             CreateObject(objectData);
         }
@@ -44,14 +34,32 @@ public sealed class ObjCreator : MonoBehaviour
     //creates a single object
     public void CreateObject(string [] objectData)
     {
-        ShowSelectObjFileDialogue(objectData);
+        UnityEngine.Debug.Log(objectData);
+        if (HasObjectType(objectData[0]))
+        {
+            CreateObjectFromLibrary(objectData);
+        }
+        else
+        {
+            ShowSelectObjFileDialogue(objectData);
+        }
     }
 
     private void CreateObjectFromFile(string filePath, string[] objectData)
     {
         UnityEngine.Debug.Log("Creating object from file");
-        loadedObject = new OBJLoader().Load(filePath);
-        //importLibrary.Add(filePath, this.filePath);
+        loadedObject = objLoader.Load(filePath);
+        if (!HasObjectType(objectData[0]))
+        {
+            importLibrary.Add(objectData[0], loadedObject.transform.GetChild(0).gameObject);
+        }
+        SetObjectProperties(objectData);
+    }
+
+    private void CreateObjectFromLibrary(string[] objectData)
+    {
+        UnityEngine.Debug.Log("Creating object from library");
+        loadedObject = Instantiate(importLibrary[objectData[0]]);
         SetObjectProperties(objectData);
     }
 
@@ -59,14 +67,14 @@ public sealed class ObjCreator : MonoBehaviour
     {
         UnityEngine.Debug.Log("Showing dialogue");
         FileBrowser.SetFilters(false, ".obj");
-        StartCoroutine( ShowSelectObjFileDialogueCoroutine(objectData) );
-        
+        StartCoroutine(ShowSelectObjFileDialogueCoroutine(objectData));
     }
 
     IEnumerator ShowSelectObjFileDialogueCoroutine(string[] objectData)
     {
         UnityEngine.Debug.Log("Running coroutine");
-        yield return FileBrowser.WaitForLoadDialog( FileBrowser.PickMode.Files, false, null, null, "Load", "Select" );
+        string loadString = "Select model for " + objectData[0];
+        yield return FileBrowser.WaitForLoadDialog( FileBrowser.PickMode.Files, false, null, null, loadString, "Import" );
         if (FileBrowser.Success)
         {
             CreateObjectFromFile(FileBrowser.Result[0], objectData);
@@ -86,14 +94,8 @@ public sealed class ObjCreator : MonoBehaviour
         //set position
         string[] coords = objectData[2].Split(' ');
         loadedObject.transform.position = new Vector3(int.Parse(coords[0]), int.Parse(coords[1]), int.Parse(coords[2]));
+        loadedObject.transform.GetChild(0).name = objectData[0];
     }
-
-    //function to check if object type has been seen before
-    /*private bool IsNewType(string objType)
-    {
-        return importLibrary.ContainsKey(objType);
-    }
-    */
 
     //to delete once file select dialogue is implemented
     public void SetString(string str)
@@ -105,6 +107,12 @@ public sealed class ObjCreator : MonoBehaviour
     private void CancelledImportHandler()
     {
         UnityEngine.Debug.Log("Import cancelled");
+    }
+
+    private bool HasObjectType(string objectType)
+    {
+        UnityEngine.Debug.Log(importLibrary.ContainsKey(objectType));
+        return importLibrary.ContainsKey(objectType);
     }
 }
 }
--- VRSBUTBI/Assets/ObjectCreatorTester.cs
+++ VRSBUTBI/Assets/ObjectCreatorTester.cs
@@ -9,7 +9,7 @@ public class ObjectCreatorTester : MonoBehaviour
     //remember backslashes need to be escaped
     //string filePath = string.Empty; 
     // example list of object data
-    string[,] objects = new string[,] {{"ObjectType", "ObjectName1", "0 0 0"}};
+    string[,] objects = new string[,] {{"ObjectType", "ObjectName1", "0 0 0"}, {"ObjectType", "ObjectName2", "5 5 5"}};
     // Start is called before the first frame update
     void Start()
     {
