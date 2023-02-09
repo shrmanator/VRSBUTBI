ea6d2287d3dc4df2bdc9a0a1bb40f8c2dd87dca1
shrmanator <dovindustries@gmail.com>
2023-02-09 13:21:27 -0500

    Commits discarded

---
+++ VRSBUTBI/Assets/Scripts/GUI/InGame/InGameButtons.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls the display of the save and load buttons
+/// </summary>
+public class InGameButtons : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        /// <summary>
+        /// Displays a button that, when clicked, will save the current game state
+        /// </summary>
+        if (GUI.Button(new Rect(10, 10, 100, 30), "Save"))
+        {
+            LoadAndSaveManager.Instance.SaveGame();
+        }
+
+        /// <summary>
+        /// Displays a button that, when clicked, will load a previously saved game state
+        /// </summary>
+        if (GUI.Button(new Rect(120, 10, 100, 30), "Load File"))
+        {
+            LoadAndSaveManager.Instance.LoadGame();
+        }
+    }
+}
