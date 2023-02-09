2d04b59d5cefa5a07a6d5876d84f7c5b9771f468
shrmanator <dovindustries@gmail.com>
2023-02-09 12:51:57 -0500

    Buttons and LoadAndSaveManager scripts created

---
+++ VRSBUTBI/Assets/Scripts/StateManager/LoadAndSaveManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+/// <summary>
+/// Implements the load and save functions
+/// </summary>
+public class LoadAndSaveManager : MonoBehaviour
+{
+    /// <summary>
+    /// Instance of the LoadAndSaveManager to allow for easy access in other scripts.
+    /// </summary>
+    public static LoadAndSaveManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Saves the current state of the game.
+    /// </summary>
+    public void SaveGame()
+    {
+        // Code for saving the game state
+    }
+
+    /// <summary>
+    /// Loads a saved state of the game.
+    /// </summary>
+    public void LoadGame()
+    {
+        // Code for loading the game state
+    }
+}
