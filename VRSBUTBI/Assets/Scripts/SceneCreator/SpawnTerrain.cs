086204ea3a88f360a41b6074c02895e335a11808
shrmanator <dovindustries@gmail.com>
2023-02-10 16:11:02 -0500

    Added a public method to create a terrain obj

--- VRSBUTBI/Assets/Scripts/SceneCreator/SpawnTerrain.cs
+++ VRSBUTBI/Assets/Scripts/SceneCreator/SpawnTerrain.cs
@@ -2,21 +2,27 @@ using UnityEngine;
 using UnityEditor;
 using System.IO;
  
-
+/// <summary>
+/// Spawns the terrain on simulation start
+/// </summary>
 public class SpawnTerrain : MonoBehaviour
 {
     void Start()
-    {
-         // create a new terrain data
-        TerrainData _terrainData = new TerrainData();  
-        // set terrain width, height, length
-        _terrainData.size = new Vector3(20, 1, 20); 
-        // create a terrain with the terrain data
-        GameObject _terrain = Terrain.CreateTerrainGameObject(_terrainData);    
+    {   
+        // A terrain object with 200 width, 1 height, 200 length.
+        GameObject terrain = createTerrain(200, 1, 200);
     }
 
-    void Update()
+    /// <summary>
+    /// Returns a new terrain object given the the width, height, length. 
+    /// </summary>
+    public GameObject createTerrain(float width, float height, float length)
     {
-    
+        // Create a new terrain data.
+        TerrainData terrainData = new TerrainData();  
+        // Set terrain width, height, length.
+        terrainData.size = new Vector3(width, height, length); 
+        // Return a new terrain object with the terrain data.
+        return Terrain.CreateTerrainGameObject(terrainData);    
     }
 }
