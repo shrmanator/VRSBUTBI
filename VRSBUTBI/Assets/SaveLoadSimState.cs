e247daf742d9c5283f81749675f2d493ffd473dd
shrmanator <dovindustries@gmail.com>
2023-02-13 16:50:34 -0500

    Fixed error preventing button from showing dialog

---
+++ VRSBUTBI/Assets/SaveLoadSimState.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using SimpleFileBrowser;
+
+public class SaveLoadSimState : MonoBehaviour
+{
+    public delegate void OnSuccess(string[] filePaths);
+    public delegate void OnCancel();
+
+    public void OpenSaveDialog()
+    {
+        FileBrowser.ShowSaveDialog(OnSaveSuccess, OnSaveCancel,  FileBrowser.PickMode.Files, false, null, "new_file.txt", "Save File", "Save");
+    }
+
+    private void OnSaveSuccess(string[] filePaths)
+    {
+        Debug.Log("Selected file: " + filePaths[0]);
+    }
+
+    private void OnSaveCancel()
+    {
+        Debug.Log("Save cancelled.");
+    }
+}
--- VRSBUTBI/Assets/Scripts/GUI/InGame/InGameButtons.cs
+++ VRSBUTBI/Assets/Scripts/GUI/InGame/InGameButtons.cs
@@ -7,22 +7,25 @@ using UnityEngine;
 /// </summary>
 public class InGameButtons : MonoBehaviour
 {
+
+    private SaveLoadSimState saveLoadSimState;
+    
+    private void Start()
+    {
+        GameObject saveLoadSimStateObject = GameObject.Find("StateManager");
+        saveLoadSimState = saveLoadSimStateObject.GetComponent<SaveLoadSimState>();
+    }
+
     private void OnGUI()
     {
         /// <summary>
         /// Displays a button that, when clicked, will save the current game state
         /// </summary>
-        if (GUI.Button(new Rect(10, 10, 100, 30), "Save"))
-        {
-            LoadAndSaveManager.Instance.SaveGame();
-        }
+        if (GUI.Button(new Rect(150, 105, 100, 30), "Save")) {saveLoadSimState.OpenSaveDialog();}
 
         /// <summary>
         /// Displays a button that, when clicked, will load a previously saved game state
         /// </summary>
-        if (GUI.Button(new Rect(120, 10, 100, 30), "Load File"))
-        {
-            LoadAndSaveManager.Instance.LoadGame();
-        }
+        if (GUI.Button(new Rect(270, 105, 100, 30), "Load File")) {saveLoadSimState.OpenSaveDialog();}
     }
 }
