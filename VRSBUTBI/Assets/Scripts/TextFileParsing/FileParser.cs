0187476e20d1dff941674dc08f8057bd8f81d921
shrmanator <dovindustries@gmail.com>
2023-02-14 08:54:47 -0500

    Adding xml commenting to FileParser.cs

--- VRSBUTBI/Assets/Scripts/TextFileParsing/FileParser.cs
+++ VRSBUTBI/Assets/Scripts/TextFileParsing/FileParser.cs
@@ -1,14 +1,27 @@
 using UnityEngine;
 using System.IO;
-public class FileParser : MonoBehaviour{
+
+/// <summary>
+/// This class is responsible for parsing a file loaded by the SimFileHandler
+/// </summary>
+public class FileParser : MonoBehaviour
+{
     private void Start()
     {
+        // Subscribe to the FileLoaded event of the SimFileHandler
         SimFileHandler.FileLoaded += ParseFile;
     }
-    
+
+    /// <summary>
+    /// This method is called when the SimFileHandler's FileLoaded event is raised. It reads the contents of the file and logs it to the console.
+    /// </summary>
+    /// <param name="filePath">The path of the file that was loaded by the SimFileHandler</param>
     private void ParseFile(string filePath)
     {
+        // Read the contents of the file
         string fileText = File.ReadAllText(filePath);
+
+        // Log the contents of the file to the console
         Debug.Log("Contents of the file: " + fileText);
     }
 }
