fd16305e89fade2435c556c3615913dd7b58e58d
ChrisPark <83894916+dhcp0327@users.noreply.github.com>
2023-02-14 12:40:49 +0900

    Updated TextParser

--- VRSBUTBI/Assets/Scripts/textparser.cs
+++ VRSBUTBI/Assets/Scripts/textparser.cs
@@ -1,54 +1,14 @@
-using System.IO;
 using UnityEngine;
-using Newtonsoft.Json;
-using System.Collections.Generic;
-
-/// <summary>
-/// This class reads and parses the commands in a text file,
-/// and stores the data in a JSON format to be used by other scripts.
-/// </summary>
-
-public class TextParser : MonoBehaviour{
-
-/// <summary>    
-/// A class to store the data for each command.    
-/// </summary>    
-
-    private class CommandData    
-    {
-        public string Type;
-        public string ObjectName;
-        public string MasterName;
-        public float X;
-        public float Y;
-    }
-
-    // A list to store all the CommandData objects
-    private List<CommandData> commandDataList = new List<CommandData>();
-
-    // A public property to access the list of CommandData objects as a JSON string
-    public string CommandDataJson { get; private set; }
-
+using System.IO;
+public class TextParser : MonoBehaviour
+{
     private void Start()
     {
-        // Read the text file
-        string[] lines = File.ReadAllLines("file.txt");
-        // Parse the lines into CommandData objects
-        foreach (string line in lines)
-        {
-            string[] words = line.Split(' ');
-            if (words[0] == "CREATE")
-            {
-                CommandData cmd = new CommandData();
-                cmd.Type = words[0];
-                cmd.ObjectName = words[1];
-                cmd.MasterName = words[2];
-                cmd.X = float.Parse(words[3]);
-                cmd.Y = float.Parse(words[4]);
-                commandDataList.Add(cmd);
-            }
-        }
-        // Serialize the list of CommandData objects to a JSON string
-        CommandDataJson = JsonConvert.SerializeObject(commandDataList);
+        SimFileHandler.FileLoaded += ParseFile;
+    }
+    private void ParseFile(string filePath)
+    {
+        string fileText = File.ReadAllText(filePath);
+        Debug.Log("Contents of the file: " + fileText);
     }
 }
