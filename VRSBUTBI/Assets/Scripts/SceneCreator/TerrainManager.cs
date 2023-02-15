d09681e32a28009136159643d3e3883663a7a9bc
shrmanator <dovindustries@gmail.com>
2023-02-15 11:03:20 -0500

    Added commenting.

--- VRSBUTBI/Assets/Scripts/SceneCreator/TerrainManager.cs
+++ VRSBUTBI/Assets/Scripts/SceneCreator/TerrainManager.cs
@@ -12,11 +12,13 @@ public class TerrainManager : MonoBehaviour
     void Start()
     {   
         // A terrain object with 200 width, 1 height, 200 length.
-        GameObject terrain = createTerrain(200, 1, 200);
+        GameObject terrain = createTerrain(500, 1, 500);
     }
 
     /// <summary>
-    /// Returns a new terrain object given the the width, height, length. 
+    /// Returns a new terrain object given the the width, height, length.
+    /// Note: Since there are no textures in the project yet, this method
+    /// doesn't add any textures to the terrain.
     /// </summary>
     public GameObject createTerrain(float width, float height, float length)
     {
@@ -24,7 +26,28 @@ public class TerrainManager : MonoBehaviour
         TerrainData terrainData = new TerrainData();  
         // Set terrain width, height, length.
         terrainData.size = new Vector3(width, height, length); 
-        // Return a new terrain object with the terrain data.
-        return Terrain.CreateTerrainGameObject(terrainData);    
+
+        // Load the concrete texture
+        Texture2D texture = Resources.Load<Texture2D>("Concrete");
+
+        // Set the splat prototypes to use the concrete texture
+        SplatPrototype[] splats = new SplatPrototype[1];
+        splats[0] = new SplatPrototype();
+        splats[0].texture = texture;
+        splats[0].tileSize = new Vector2(50, 50);
+        splats[0].tileOffset = Vector2.zero;
+        terrainData.splatPrototypes = splats;
+
+        // Create a new material with the concrete texture
+        Material terrainMaterial = new Material(Shader.Find("Nature/Terrain/Diffuse"));
+        terrainMaterial.mainTexture = texture;
+
+        // Create a new terrain object
+        Terrain terrain = Terrain.CreateTerrainGameObject(terrainData).GetComponent<Terrain>();
+        terrain.materialTemplate = terrainMaterial;
+
+        // Return the terrain object
+        return terrain.gameObject;  
     }
+
 }
