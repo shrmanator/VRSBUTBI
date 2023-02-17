681d817f0941e944662c5e51ee8da741acb9bb82
ChrisPark <83894916+dhcp0327@users.noreply.github.com>
2023-02-18 01:36:38 +0900

    Updated TextParser separating commands

--- VRSBUTBI/Assets/Scripts/TextFileParsing/FileParser.cs
+++ VRSBUTBI/Assets/Scripts/TextFileParsing/FileParser.cs
@@ -1,64 +1,54 @@
 using UnityEngine;
 using System.IO;
+using System.Collections.Generic;
 
-
-/// <summary>
-/// This class is responsible for parsing an input file, organizing the data into a list of objects,
-/// and saving the data to a JSON file. 
-/// 
-/// It subscribes to the FileLoaded event of the SimFileHandler (see SimFileHandler.cs) and raises the
-///  JsonFileCreated event (see the start method) when the JSON file is created.
-/// </summary>
 public class FileParser : MonoBehaviour
 {
-    // Define a class or struct to hold the data from the input file
-    [System.Serializable]
-    private class Data
-    {
-        public string name;
-        public int score;
-    }
-
-    // Define a delegate and event for the JSON file created event
-    public delegate void JsonFileCreatedHandler(string filePath);
-    public static event JsonFileCreatedHandler JsonFileCreated;
-
     private void Start()
     {
         // Subscribe to the FileLoaded event of the SimFileHandler
         SimFileHandler.FileLoaded += ParseFile;
     }
 
-    /// <summary>
-    /// This method is called when the SimFileHandler's FileLoaded event is raised. It parses the
-    /// contents of the file, saves the data to a JSON file, and raises the JsonFileCreated event.
-    /// </summary>
-    /// <param name="filePath">The path of the file that was loaded by the SimFileHandler</param>
     private void ParseFile(string filePath)
     {
         // Read the contents of the file
-        string[] lines = File.ReadAllLines(filePath);
+        string fileText = File.ReadAllText(filePath);
 
-        // Parse the input file and organize the information into a list of Data objects
-        List<Data> dataList = new List<Data>();
+        // Split the contents of the file into individual lines
+        string[] lines = fileText.Split('\n');
+
+        // Create a list to hold the commands
+        List<object[]> commands = new List<object[]>();
+
+        // Parse each line into a command and add it to the list
         foreach (string line in lines)
         {
-            string[] fields = line.Split(',');
-            Data data = new Data { name = fields[0], score = int.Parse(fields[1]) };
-            dataList.Add(data);
+            if (string.IsNullOrEmpty(line)) continue; // Skip empty lines
+
+            // Split the line into its components
+            string[] components = line.Split(',');
+
+            // Parse the components and add them to the list of commands
+            switch (components[0])
+            {
+                case "CREATE":
+                    commands.Add(new object[] { components[1], components[2], int.Parse(components[3]), int.Parse(components[4]) });
+                    break;
+                case "SETOBJCELL":
+                    commands.Add(new object[] { components[1], components[2], int.Parse(components[3]), int.Parse(components[4]) });
+                    break;
+                default:
+                    Debug.LogWarning("Unrecognized command: " + components[0]);
+                    break;
+            }
         }
 
-        // Convert the list of Data objects to a JSON string
-        string json = JsonUtility.ToJson(dataList, prettyPrint: true);
-
-        // Write the JSON string to a file
-        string jsonFilePath = "data.json";
-        File.WriteAllText(jsonFilePath, json);
-
-        // Raise the JsonFileCreated event with the path of the created JSON file
-        if (JsonFileCreated != null)
-        {
-            JsonFileCreated(jsonFilePath);
-        }
+        // Raise the CommandReceived event and pass the list of commands
+        CommandReceived?.Invoke(commands);
     }
+
+    // Define the CommandReceived event
+    public delegate void CommandReceivedEventHandler(List<object[]> commands);
+    public static event CommandReceivedEventHandler CommandReceived;
 }
