8d8431586d5680432885271b305d136cc604353d
shrmanator <dovindustries@gmail.com>
2023-05-11 14:24:58 -0400

    Added the instructions via GUI.label call in TExtController and removed the text object in the GUI heirarchy

--- VRSBUTBI/Assets/TextController.cs
+++ VRSBUTBI/Assets/TextController.cs
@@ -1,40 +1,44 @@
 using UnityEngine;
 
-/// <summary>
-/// Controls the display of the instructions panel.
-/// </summary>
 public class TextController : MonoBehaviour
 {
-    /// <summary>
-    /// The GameObject for the instruction panel.
-    /// </summary>
-    public GameObject instructionPanel;
+    public GameObject instructionText;
 
-    private ScenePlayer scenePlayer;
-
-    /// <summary>
-    /// Start is called before the first frame update.
-    /// </summary>
     private void Start()
     {
-        scenePlayer = GameObject.FindObjectOfType<ScenePlayer>();
-        instructionPanel.SetActive(false);
+        if (instructionText != null)
+        {
+            instructionText.SetActive(false);
+        }
     }
 
-    /// <summary>
-    /// Update is called once per frame.
-    /// </summary>
-    void Update()
+    void OnGUI()
     {
-        // Show the instructions when the game is in the run state and there is no scene playing
-        if (!scenePlayer.IsSceneLoaded() && !scenePlayer.IsScenePlaying())
+        #if UNITY_EDITOR
+        if (!Application.isPlaying) return;
+        #endif
+
+        if (!ScenePlayer.Player.IsScenePlaying())
         {
-            instructionPanel.SetActive(true);
+            GUIStyle style = new GUIStyle(GUI.skin.label);
+            style.alignment = TextAnchor.MiddleCenter;
+            style.fontSize = 24;
+            GUI.Label(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 40, 400, 80),
+                "Please import a text file or load a simulation state, then press \"Start Simulation\"",
+                style);
         }
-        else
+    }
+
+
+    private void Update()
+    {
+        if (Application.isPlaying && instructionText != null && !ScenePlayer.Player.IsScenePlaying())
         {
-            // Hide the instructions
-            instructionPanel.SetActive(false);
+            instructionText.SetActive(true);
+        }
+        else if (instructionText != null)
+        {
+            instructionText.SetActive(false);
         }
     }
 }
