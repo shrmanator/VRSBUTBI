f74ffbf4ad0ae7bcadec0f72fa6ea0825859d205
shrmanator <dovindustries@gmail.com>
2023-05-16 16:23:03 -0400

    Revert "added cillisiondecectionmode as continuousspeculative;."
    This reverts commit 96bf08e49583e5c4770218aa7e68fd21a38a8e25.

--- VRSBUTBI/Assets/Scripts/ObjectManager/ObjectManager.cs
+++ VRSBUTBI/Assets/Scripts/ObjectManager/ObjectManager.cs
@@ -236,8 +236,6 @@ public sealed class ObjectManager : MonoBehaviour
             rb.angularDrag = 0.05f;
             rb.useGravity = true;
             rb.isKinematic = false;
-            rb.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
-
         }
         _loadedObject.tag = "Serializable";
         _loadedObject.SetActive(true);
